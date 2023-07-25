be1837bcc9b9c2cf7b2ba7621861f8418a481cdf
Juliana <julianalimaprado123@gmail.com>
2023-07-25 12:25:11 -0300

    Adicionado menu responsivo do index

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-function logout() {
+function login() {
     window.location.href = "pages/login/login.html";
 
+}
+
+function criar() {
+    window.location.href = "pages/register/register.html";
+
+}
+
+function subMenu() {
+    const sub = document.querySelector(".sub-menu");
+    sub.style.display = "block";
+
+    const barra = document.querySelector(".barra-de-menu");
+    barra.style.display = "none";
+
+    const img = document.querySelector(".logo");
+    img.style.display = "none";
+
+    
+    const header = document.querySelector(".header");
+    header.style.display = "block";
+}
+
+function fecharMenu() {
+    const sub = document.querySelector(".sub-menu");
+    sub.style.display = "none";
+
+    const barra = document.querySelector(".barra-de-menu");
+    barra.style.display = "block";
+
+    const img = document.querySelector(".logo");
+    img.style.display = "block";
+
+    const header = document.querySelector(".header");
+    header.style.display = "flex";
 }
