f3a9146677e26312d72d931132305fc22258884a
Juliana <julianalimaprado123@gmail.com>
2023-07-28 09:54:17 -0300

    Layout Responsito da Tela de Login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,18 +1,24 @@
 /*Inicializa executando o comando. Se o usuário estiver logado, continuar na página de home */
-window.onload = function() {
+window.onload = function () {
     firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        window.location.href = "../home/home.html";
-      }
-    })};
+        if (user) {
+            window.location.href = "../home/home.html";
+        }
+    })
+};
+
+
+function abrirHome() {
+    window.location.href = "../../index.html";
+}
 
 /*Verifica se o email é válido para habilitar o recuperar senha*/
-function onChangeEmail(){
+function onChangeEmail() {
     toggleButtonDisable();
     toggleEmailErrors();
 }
 
-function onChangePassword(){
+function onChangePassword() {
     toggleButtonDisable();
     togglePasswordErrors();
 }
@@ -35,25 +41,25 @@ function login() {
 }
 
 /*O que acontece com a mensagem de erro anterior: Se não encontrar o usuário, dirá ou se a senha não for igual dirá também */
-function getErrorMessage(error){
-        if (error.code =="auth/user-not-found") {
-            return "Usuário não encontrado";
-        }
-        if (error.code == "auth/wrong-password"){
-            return "Senha inválida"
-        }
-        return error.message;
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not-found") {
+        return "Usuário não encontrado";
+    }
+    if (error.code == "auth/wrong-password") {
+        return "Senha inválida"
+    }
+    return error.message;
 }
 
 /*Se chamar o registro vai para tela de registro */
-function register(){
+function register() {
     window.location.href = "../register/register.html"
 }
 
 /*Se o usuário esqueceu a senha receberá uma mensagem no email para recuperar*/
-function recoverPassword(){
+function recoverPassword() {
     showLoading();
-    
+
     firebase.auth().sendPasswordResetEmail(document.getElementById("email").value).then(() => {
         hideLoading();
         alert('Email enviado com sucesso!');
@@ -64,7 +70,7 @@ function recoverPassword(){
 }
 
 /*Função que valida o email, lógica escrita no validation.js */
-function isEmailValid(){
+function isEmailValid() {
     const email = document.getElementById("email").value;
     if (!email) {
         return false;
@@ -73,45 +79,47 @@ function isEmailValid(){
 }
 
 /*Função que valida o senha*/
-function isPasswordValid(){
+function isPasswordValid() {
     const password = document.getElementById('password').value;
-    if (!password){
+    if (!password) {
         return false;
     }
     return true;
 }
 
 /*Vai definir se o botão vai aparecer caso exista um erro ou não no email*/
-function toggleEmailErrors(){
+function toggleEmailErrors() {
     const email = document.getElementById('email').value;
-    if(!email){
+    if (!email) {
         document.getElementById('email-required-error').style.display = "block";
-    } else{
+    } else {
         document.getElementById('email-required-error').style.display = "none";
     }
 
-    if (validateEmail(email)){
+    if (validateEmail(email)) {
         document.getElementById('email-invalid-error').style.display = "none";
-    }else{
+    } else {
         document.getElementById('email-invalid-error').style.display = "block";
     }
 }
 
 /*Vai definir se o botão vai aparecer caso exista um erro ou não na senha*/
-function togglePasswordErrors(){
+function togglePasswordErrors() {
     const password = document.getElementById('password').value;
-    if(!password){
+    if (!password) {
         document.getElementById('password-required-error').style.display = "block";
-    } else{
+    } else {
         document.getElementById('password-required-error').style.display = "none";
     }
 }
 
 /*Vai definir se o botão vai aparecer caso exista um erro ou não na senha ou email*/
-function toggleButtonDisable(){
+function toggleButtonDisable() {
     const emailValid = isEmailValid();
     document.getElementById("recover-password-button").disabled = !emailValid;
 
     const passwordValid = isPasswordValid();
     document.getElementById('login-button').disabled = !emailValid || !passwordValid;
 }
+
+
